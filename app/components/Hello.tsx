7dc5a2889477744a919641b3477e02ea794351d0
Mary Rose Cook <mary@maryrosecook.com>
2025-06-09 12:57:11 -0700

    add shadcn support

--- app/components/Hello.tsx
+++ app/components/Hello.tsx
@@ -1,3 +1,9 @@
+import { Button } from "./ui/button";
+
 export function Hello() {
-  return <div data-testid="hello">Hello</div>;
+  return (
+    <div data-testid="hello" className="">
+      Hello <Button>Click me</Button>
+    </div>
+  );
 }
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   test: {
     environment: "jsdom",
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./app"),
+    },
   },
 });
